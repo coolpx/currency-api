58cabb320b2d872d0efa2bb31a4875ab837ac626
coolpx <coolp_xels@proton.me>
2022-05-12 19:55:33 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CoolPixels Currency</title>
+        <link rel="stylesheet" href="/.css">
+    </head>
+    <body>
+        <h1>CoolPixels Currency</h1>
+        <p>
+            This is a little project I made to see how it would develop. It's really only an API to transfer a currency between accounts. But I really wanted to see what happens when you make a currency API that anybody can use. Get started by <a href="/register.php">registering</a> and <a href="/documentation.php">reading the documentation</a>.
+        </p>
+        <p>
+            
+        </p>
+    </body>
+</html>
