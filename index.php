2a3a5c47662e3b2e42149f88d046604a7cc4ee98
coolpx <coolp_xels@proton.me>
2022-05-12 21:19:46 -0500

    slightly change text on index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <body>
         <h1>CoolPixels Currency</h1>
         <p>
-            This is a little project I made to see how it would develop. It's really only an API to transfer a currency between accounts. But I really wanted to see what happens when you make a currency API that anybody can use. Get started by <a href="/register.php">registering</a> and <a href="/documentation.php">reading the documentation</a>.
+            This is a little project I made to see how a fake currency would develop. It's really only an API to transfer a currency between accounts. But I really wanted to see what happens when you make a currency API that anybody can use. Get started by <a href="/register.php">registering</a> and <a href="/documentation.php">reading the documentation</a>.
         </p>
         <p>
             
